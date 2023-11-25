9ce00f6b25e78dae813a4fe7c0527b3c5e49c27d
FrogHills <froghillsdevs@gmail.com>
2023-11-25 15:38:36 -0300

    update landing page

--- FirstSemester/ALP/lista-exercicios-vetores-matrizes/main.c
+++ FirstSemester/ALP/lista-exercicios-vetores-matrizes/main.c
@@ -110,11 +110,238 @@ int main()
     */
 
     //Ex 4
+    /*
     char type[3][20] = {"Unitário", "Duplo", "10 Viagens"};
-    float price[3] = {1.30, 2.60, 12.00};
+    float price[3] = {1.30, 2.60, 12.00}, pagamento = 0.00;
+    int escolhido = 0;
 
-    printf("Qual passagems pretende comprar?")
+    printf("Qual passagems pretende comprar?\n");
+    printf("------------------------------\n");
+    printf("|   1:%s: %.2f         |\n",type[0], price[0]);
+    printf("|   2:%s: %.2f            |\n",type[1], price[1]);
+    printf("|   3:%s: %.2f      |\n",type[2], price[2]);
+    printf("------------------------------\n");
 
+    printf("Escolha sua passagem:\n");
+    scanf("%d", &escolhido);
+    printf("Quanto crédito você tem para as passagens?\n");
+    scanf("%f", &pagamento);
+
+    switch(escolhido)
+    {
+        case 1:
+
+            if(pagamento > price[0])
+            {
+                int quantidade = pagamento/price[0];
+                float troco = pagamento - quantidade*price[0];
+                printf("Você pode comprar %d unitário\n", quantidade);
+                printf("Seu troco será: R$ %.2f", troco);
+            }
+
+        break;
+
+        case 2:
+
+            if(pagamento > price[1])
+            {
+                int quantidade = pagamento/price[1];
+                float troco = pagamento - quantidade*price[1];
+                printf("Você pode comprar %d Duplo\n", quantidade);
+                printf("Seu troco será: R$ %.2f", troco);
+            }
+
+        break;
+
+        case 3:
+
+            if(pagamento > price[2])
+            {
+                int quantidade = pagamento/price[2];
+                float troco = pagamento - quantidade*price[2];
+                printf("Você pode comprar %d 10 Viagens\n", quantidade);
+                printf("Seu troco será: R$ %.2f", troco);
+            }
+
+        break;
+
+        default:
+            printf("Passagem inválida");
+        break;
+    }
+    */
+
+    //Ex 5
+    /*
+    int Vetor[12] = {1,2,3,4,5,6,7,8,9,10,11,12}, X = 0, Y = 0;
+
+    printf("Faça a soma dos números do array:\n");
+    printf("Escolha a posição X:\n");
+    scanf("%d", &X);
+    printf("Escolha a posição Y:\n");
+    scanf("%d", &Y);
+
+    printf("A soma das posições X:%d e Y:%d\n", X, Y);
+    printf("%d\n", Vetor[X]+Vetor[Y]);
+    */
+
+    //Ex 6
+    /*
+    int Vetor[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}, X;
+    printf("Qual Valor está procurando?: \n");
+    scanf("%d", &X);
+
+    for(int i = 0; i <= 20; i++)
+    {
+        if(Vetor[i] == X)
+        {
+            printf("O VALOR PROCURADO ESTÁ NO ARRAY\n");
+            printf("Valor: %d\n", X);
+            printf("Posição: %d\n", i);
+        }
+    }
+
+    if(X < 20 || X < 1)
+    {
+       printf("Valor inválido");
+    }
+    */
+
+    //Ex 7
+    /*
+    char Vetor[20];
+    char Vogais[10] = {'a','e','i','o','u', 'A', 'E', 'I', 'O', 'U'};
+    int CountV = 0, CountC = 0;
+
+    printf("Escreva uma palavra: \n");
+    fgets(Vetor, sizeof(Vetor), stdin);
+
+    printf("Cada letra da palavra: \n");
+    for(int i = 0; i < strlen(Vetor); i++)
+    {
+        if(isalpha(Vetor[i])) {
+            int ehVogal = 0;
+            for(int j = 0; j < 10; j++) {
+                if (Vetor[i] == Vogais[j]) {
+                    ehVogal = 1;
+                    CountV += 1;
+                    break;
+                }
+            }
+
+            if (!ehVogal) {
+                CountC += 1;
+            }
+        }
+    }
+
+    printf("O total de Vogais é: %d\n", CountV);
+    printf("O total de Consoantes é: %d\n", CountC);
+    */
+
+    //Ex 8
+    /*
+    int tamanho;
+
+    printf("Digite o tamanho da string: ");
+    scanf("%d", &tamanho);
+
+    if (tamanho <= 0) {
+        printf("Tamanho inválido. O tamanho deve ser maior que zero.\n");
+        return 1;
+    }
+
+    char Vetor[tamanho];
+
+    printf("Digite a string: ");
+    scanf(" %[^\n]s", Vetor);
+    int i, j;
+    char temp;
+    for (i = 0, j = tamanho - 1; i < j; i++, j--) {
+        temp = Vetor[i];
+        Vetor[i] = Vetor[j];
+        Vetor[j] = temp;
+    }
+
+    printf("String invertida: %s\n", Vetor);
+    */
+
+    //Ex 9
+    /*
+    int matriz[3][3];
+    int soma = 0;
+
+    printf("Digite os elementos da matriz 3x3:\n");
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Elemento [%d][%d]: ", i + 1, j + 1);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            soma += matriz[i][j];
+        }
+    }
+
+    printf("Matriz inserida:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d\t", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("A soma dos elementos da matriz é: %d\n", soma);
+    */
+
+    //Ex 10
+    /*
+    int N;
+    printf("Digite o tamanho da matriz (N x N): ");
+    scanf("%d", &N);
+
+    if (N <= 0) {
+        printf("Tamanho inválido. O tamanho deve ser maior que zero.\n");
+        return 1;
+    }
+
+    int matriz[N][N];
+    int valorProcurado;
+
+    printf("Digite os elementos da matriz %dx%d:\n", N, N);
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("Elemento [%d][%d]: ", i + 1, j + 1);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    printf("Digite um valor para procurar na matriz: ");
+    scanf("%d", &valorProcurado);
+
+    int encontrado = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (matriz[i][j] == valorProcurado) {
+                encontrado = 1;
+                break;
+            }
+        }
+        if (encontrado) {
+            break;
+        }
+    }
+
+    if (encontrado) {
+        printf("Valor encontrado.\n");
+    } else {
+        printf("Valor não encontrado.\n");
+    }
+    */
 
     return 0;
 }
